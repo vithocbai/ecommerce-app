5c060e30d9b9d44de34bbc4384d4b2081a360bf6
HelloBoss <you@example.com>
2025-12-04 09:57:02 +0700

    Css cho giao diện quickview #2

--- src/components/Shop/QuickView/QuickViewModal.tsx
+++ src/components/Shop/QuickView/QuickViewModal.tsx
@@ -1,7 +1,22 @@
-import { X, Eye, RefreshCcw, Heart, Twitter, Facebook, Linkedin, PhoneOutgoing, ChevronUp } from "lucide-react";
+import {
+    X,
+    Eye,
+    RefreshCcw,
+    Heart,
+    Twitter,
+    Facebook,
+    Linkedin,
+    PhoneOutgoing,
+    ChevronUp,
+    ChevronDown,
+    Minus,
+    Plus,
+    ShoppingBasket,
+} from "lucide-react";
 import { renderStars } from "../../ui/renderStar";
 import { useEffect, useState } from "react";
 import { AnimatePresence, motion } from "framer-motion";
+import { AddToCart } from "../../ui/addToCart";
 
 type Props = {
     isOpen: boolean;
@@ -9,6 +24,8 @@ type Props = {
 };
 
 export const QuickViewModal = ({ isOpen, onClose }: Props) => {
+    const [showMoreDetail, setShowMoreDetail] = useState(false);
+
     return (
         <section>
             <AnimatePresence>
@@ -32,7 +49,7 @@ export const QuickViewModal = ({ isOpen, onClose }: Props) => {
                                 animate={{ x: "0%" }}
                                 exit={{ x: "100%" }}
                                 transition={{ duration: 0.2, ease: "linear" }}
-                                className="fixed top-0 right-0 z-[50]  w-[400px] h-screen bg-white"
+                                className="fixed top-0 right-0 z-[50] w-[400px] h-screen bg-white p-[15px] overflow-y-auto overflow-x-hidden"
                             >
                                 {/* Close Button */}
                                 <div
@@ -42,8 +59,8 @@ export const QuickViewModal = ({ isOpen, onClose }: Props) => {
                                     <X size={14} />
                                 </div>
                                 {/* Content */}
-                                <div>
-                                    <h3>
+                                <div className="px-[3px]">
+                                    <h3 className="block text-[24px] text-[#222] font-medium mb-[12px] leading-7">
                                         <a href="">Hi-Resolution Bluetooth 4.0 Wireless Speakers</a>
                                     </h3>
                                     {/* Image */}
@@ -55,103 +72,137 @@ export const QuickViewModal = ({ isOpen, onClose }: Props) => {
                                         <div></div>
                                     </div>
                                     {/* Price */}
-                                    <div>
+                                    <div className="text-[#888] text-[20px] mb-[12px]">
                                         <span>
                                             <del>$320.00</del>
                                         </span>
-                                        <span>$80</span>
+                                        <span className="pl-4">$80</span>
                                     </div>
                                     {/* Rating */}
-                                    <div className="flex items-center gap-[2px]">{renderStars(5)}</div>
+                                    <div className="flex items-center gap-[2px] mb-[12px]">
+                                        {renderStars(5)}
+                                        <span className="text-[18px] text-[#222]">(1 customer review)</span>
+                                    </div>
                                     {/* View people */}
-                                    <p className="flex items-center gap-[2px]">
+                                    <p className="flex items-start gap-[2px] mb-[12px] text-[16px]">
                                         <span>
                                             <Eye className="w-[16px]" />
                                         </span>
                                         19 people are viewing this product right now
                                     </p>
                                     {/* Description */}
-                                    <p>
+                                    <p className="text-[#444] text-[16px] mb-[12px]">
                                         This Bluetooth speaker delivers big sound, making it then only music system
                                         you’ll need in or out of the house. Prem materials such as anodized aluminum &
                                         durable polymers withstand the rigor of an active lifestyle.
                                     </p>
                                     {/* Add to Cart */}
-                                    <div>
-                                        <div className="flex items-center gap-[2px]">
-                                            <span>+</span>
-                                            <div>4</div>
-                                            <span>-</span>
+                                    <div className="flex items-center mb-[22px]">
+                                        <div className="flex items-center  mr-[20px] h-[38px] ">
+                                            <span className="flexCenter px-[4px] text-[#222] border-[1px] border-[#e1e1e1] border-r-[0] h-full">
+                                                <Minus strokeWidth={1.5} size={18} />
+                                            </span>
+                                            <div className="flexCenter w-[45px] h-full border-[1px] border-[#e1e1e1] ">
+                                                4
+                                            </div>
+                                            <span className="flexCenter px-[4px] text-[#222] border-[1px] border-[#e1e1e1] border-l-[0] h-full">
+                                                <Plus strokeWidth={1.5} size={18} />
+                                            </span>
                                         </div>
-                                        <button>Add to Cart</button>
+                                        <AddToCart
+                                            title="Add to card"
+                                            icon={<ShoppingBasket strokeWidth={2.5} size={18} />}
+                                        />
                                     </div>
                                     {/* Reaload */}
-                                    <div className="flex items-center gap-[2px]">
-                                        <RefreshCcw className="w-[16px]" />
-                                        <p>Add to compare</p>
+                                    <div className="flex items-center gap-[2px] mb-[10px] cursor-pointer">
+                                        <RefreshCcw size={20} strokeWidth={1.5} />
+                                        <p className="pl-[5px]">Add to compare</p>
                                     </div>
                                     {/* Heart */}
-                                    <div>
-                                        <Heart className="w-[16px]" />
-                                        <p>Add to wishlist</p>
+                                    <div className="flex items-center gap-[2px] mb-[10px] cursor-pointer">
+                                        <Heart size={20} strokeWidth={1.5} />
+                                        <p className="pl-[5px]">Add to wishlist</p>
                                     </div>
                                     {/* Skud */}
-                                    <div>
-                                        <span>skud: 1231-1</span>
+                                    <div className="text-[16px] text-[#888] mb-[12px]">
+                                        <span className="text-[#222] uppercase">sku: </span> 1231-1
                                     </div>
                                     {/* Category */}
-                                    <div>
-                                        <span>Category, Computer & PC </span>
+                                    <div className="text-[16px] text-[#888] mb-[12px]">
+                                        <span className="text-[#222] ">Category: </span> Wearable Items
                                     </div>
                                     {/* Tags */}
-                                    <div>
-                                        <span>Headphones, Speaker,</span>
+                                    <div className="text-[16px] text-[#888] mb-[12px]">
+                                        <span className="text-[#222] ">Tags: </span> Headphones, Speaker
                                     </div>
                                     {/* Share */}
-                                    <div className="flex items-center">
-                                        <span>Share:</span>
+                                    <div className="flex items-center gap-1 text-[16px] text-[#888] mb-[12px]">
+                                        <span className="text-[#222] mr-1">Share:</span>
                                         <span>
-                                            <Twitter />
+                                            <Twitter size={16} />
                                         </span>
                                         <span>
-                                            <Facebook />
+                                            <Facebook size={16} />
                                         </span>
                                         <span>
-                                            <Linkedin />
+                                            <Linkedin size={16} />
                                         </span>
                                         <span>
-                                            <PhoneOutgoing />
+                                            <PhoneOutgoing size={16} />
                                         </span>
                                     </div>
                                     {/* More Detail */}
                                     <div>
-                                        <div>
-                                            More Detail{" "}
+                                        <div
+                                            onClick={() => setShowMoreDetail(!showMoreDetail)}
+                                            className="flex items-center mb-[12px]"
+                                        >
+                                            <div className="underline">More Detail</div>
                                             <span>
-                                                <ChevronUp />
+                                                {showMoreDetail ? <ChevronUp size={18} /> : <ChevronDown size={18} />}
                                             </span>
                                         </div>
-                                        <div>
-                                            <h2>Overview</h2>
-                                            <h2>Featuring True360 Sound Technology</h2>
-                                            <p>
-                                                With its compact footprint and convenient leathers carrying handles the
-                                                Beolit 17 wireless Bluetooth speaker ready to accompany you on all of
-                                                life’s adventures Featuring 360-degrees sound technology the Beolit 17
-                                                lets you enjoying immersive Bang & Olufsen signature sound whenever and
-                                                wherever you'd like Whether you’re at home or on the road you can
-                                                transform any moment with the power clarity portable.
-                                            </p>
-                                            <img src="" alt="" />
-                                            <h2>Features</h2>
-                                            <p>
-                                                Auto Schedule - True360 Sound Technology featuring Bang & Olufsen
-                                                Signature Sound Auto Away - Premium materials are built to last Remote
-                                                Control - Connect button activates intelligent features via Bang &
-                                                Olufsen App Energy Star - Portable Bluetooth speaker keeps going ...
-                                            </p>
-                                            <div>Show full details</div>
-                                        </div>
+                                        <AnimatePresence>
+                                            {showMoreDetail && (
+                                                <motion.div 
+                                                    initial={{height: 0,opacity: 0}}
+                                                    animate={{height: 'auto' ,opacity: 1}}
+                                                    exit={{opacity: 0}}
+                                                    transition={{duration: 0.5, ease: "easeIn"}}
+                                                    style={{ overflow: "hidden"}}
+                                                >
+                                                    <h2 className="my-[8px] text-[24px] font-medium">Overview</h2>
+                                                    <h2 className="my-[8px] text-[24px] font-medium leading-7">
+                                                        Featuring True360 Sound Technology
+                                                    </h2>
+                                                    <p className="text-[#444]">
+                                                        With its compact footprint and convenient leathers carrying
+                                                        handles the Beolit 17 wireless Bluetooth speaker ready to
+                                                        accompany you on all of life’s adventures Featuring 360-degrees
+                                                        sound technology the Beolit 17 lets you enjoying immersive Bang
+                                                        & Olufsen signature sound whenever and wherever you'd like
+                                                        Whether you’re at home or on the road you can transform any
+                                                        moment with the power clarity portable.
+                                                    </p>
+                                                    <img
+                                                        src="https://xstore.8theme.com/elementor/demos/minimal-electronics/wp-content/uploads/sites/71/2022/02/Image-2.jpeg"
+                                                        alt=""
+                                                    />
+                                                    <h2 className="my-[8px] text-[24px] font-medium leading-7">
+                                                        Features
+                                                    </h2>
+                                                    <p className="text-[#444] mb-[12px]">
+                                                        Auto Schedule - True360 Sound Technology featuring Bang &
+                                                        Olufsen Signature Sound Auto Away - Premium materials are built
+                                                        to last Remote Control - Connect button activates intelligent
+                                                        features via Bang & Olufsen App Energy Star - Portable Bluetooth
+                                                        speaker keeps going ...
+                                                    </p>
+                                                    <div className="underline text-[#222]">Show full details</div>
+                                                </motion.div>
+                                            )}
+                                        </AnimatePresence>
                                     </div>
                                 </div>
                             </motion.div>
--- src/components/ui/addToCart.tsx
+++ src/components/ui/addToCart.tsx
@@ -1,9 +1,15 @@
 type addToCartProps = {
-    title: string
-}
+    title: string;
+    icon?: React.ReactNode;
+};
 
-export const AddToCart = ({title}: addToCartProps) => {
-    return <section>
-        <button className="px-[30px] py-[10px] font-bold text-[16px] bg-[#2A74ED] text-[#fff] rounded-full hover:bg-[#000] transition-all duration-200 ease-linear">{title}</button>
-    </section>
-}
+export const AddToCart = ({ title, icon }: addToCartProps) => {
+    return (
+        <section>
+            <button className="flex items-center px-[30px] py-[10px] font-bold text-[16px] bg-[#2A74ED] text-[#fff] rounded-full hover:bg-[#000] transition-all duration-200 ease-linear">
+                {icon && <span className="pr-[5px]">{icon}</span>}
+                <span>{title}</span>
+            </button>
+        </section>
+    );
+};
